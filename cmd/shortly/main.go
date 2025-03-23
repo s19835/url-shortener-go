d930b69ef0670bf45fb8f0c7c6774bdf3d91bbc5
s19835 <s19835@sci.pdn.ac.lk>
2025-03-23 22:51:11 +0530

    add: gin

--- cmd/shortly/main.go
+++ cmd/shortly/main.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"log"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	"github.com/s19835/url-shortener-go/internal/config"
 )
 
 func main() {
 	config.LoadEnv()
 	log.Println("loaded environment variables")
+
+	// set up routers
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
 }
