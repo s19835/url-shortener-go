22138623279fd5cfc19b04cf037527934595e0c2
s19835 <s19835@sci.pdn.ac.lk>
2025-03-24 20:13:12 +0530

    add: gin

--- main.go
+++ main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// create a router
+	r := gin.Default()
+
+	r.GET("/ping", func(ctx *gin.Context) {
+		log.Println("Connected")
+	})
+
+	r.Run()
+}
